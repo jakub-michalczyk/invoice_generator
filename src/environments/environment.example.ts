2b65b7f481ca82de29677579aac7433e726d4c46
Jakub <devcobbier@gmail.com>
2024-09-09 16:02:47 +0200

    change file limits, edited enviroment example file

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiKey: 'your-api-key-here',
-  jsonBinUrl: 'https://api.jsonbin.io/v3/b/66dedf2fe41b4d34e42c7693',
+  apiUrl: 'https://api.jsonbin.io/v3/b/66dedf2fe41b4d34e42c7693',
 };
